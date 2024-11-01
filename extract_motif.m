a83d67c379c232a9c31fb7395ef8550314b765df
hoang-atr <55073871+hoang-atr@users.noreply.github.com>
2024-11-01 15:34:16 +0900

    Update extract_motif.m

--- extract_motif.m
+++ extract_motif.m
@@ -1,23 +1,53 @@
-function [num_motif,motif_idx,num_iso_cluster] = extract_motif(S,p,min_burst)
+function motif_idx = extract_motif(S,p,min_burst)
+%% input 
+% S: - similarity matrix; 
+% p:  preference parameter of Affinity Propagation clustering
+% min_burst: minimum burst size of clusters to be considered as motifs
+%% output
+% motif_idx: index of bursts that belong to the repeating motifs
+% Note: size of <motif_idx> is the number of repeating motifs
+Nburst = size(S,1);
+
 if nargin<2, p=median(S(:)); end
-if nargin<3, min_burst = sqrt(size(S,1)); end
+if nargin<3, min_burst = sqrt(Nburst); end
 
-[idx,netsim,dpsim,expref]=apcluster(S,p,'maxits',1000,'convits',100,'dampfact',0.7,'nonoise');
+%% Affinity Propagation clustering
+idx=apcluster(S,p,'maxits',1000,'convits',100,'dampfact',0.7,'nonoise');
 
+%% check the cluster size
 uidx = unique(idx);
-
-motif_idx = cell(1,1);
-num_motif = 0;
-num_iso_cluster = 0;
+cluster_idx = cell(1,1);
+num_cluster = 0;
 for i = 1:numel(uidx)
     ix = find(idx==uidx(i));
     
     if numel(ix)>min_burst
-        num_motif = num_motif + 1;
-        motif_idx{num_motif} = ix;
-    end
+        num_cluster = num_cluster + 1;
+        cluster_idx{num_cluster} = ix;
+    end    
+end
+
+%% check the separation between clusters
+% similarity within-cluster
+Swi = cell(num_cluster,1);
+for i = 1:num_cluster
+    idx = cluster_idx{i};
+    Sm = S(idx,idx);
+    ix = triu(Sm,1)>eps; 
     
-    if numel(ix)<2
-        num_iso_cluster = num_iso_cluster+1;
+    Swi{i} = Sm(ix);
+end
+
+% check the separation between within- and across-cluster
+motif = ones(num_cluster,1);
+for i = 1:num_cluster
+    idx = cluster_idx{i};
+    idx_k = setdiff(1:Nburst,idx);
+    Sac = S(idx,idx_k); % similarity across-cluster
+    [~,p] = ttest2(Sac(:),Swi{i},'tail','right');
+    if p < 0.01
+        motif(i) = 0;        
     end
 end
+
+motif_idx=cluster_idx(motif>0);
